efd154d14d41967ef0a9c939c1a80b348b4a982f
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-06-12 00:17:09 +0200

    ReactiveFormsModule - soumission du formulaire

--- CaveAVinClient/src/app/composants/formulaires/form-region/form-region.component.ts
+++ CaveAVinClient/src/app/composants/formulaires/form-region/form-region.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
+import {RegionService} from "../../../services/region.service";
+import {Region} from "../../../modeles/region";
 
 @Component({
   selector: 'app-form-region',
@@ -8,13 +10,18 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class FormRegionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private serviceRegion: RegionService) { }
 
   regionForm = new FormGroup({
     nom : new FormControl('')
   })
 
   ngOnInit(): void {
+  }
+
+  ajoutRegion(region:Region){
+    this.serviceRegion.addRegion(region);
+    console.log(this.serviceRegion.getRegions())
   }
 
 }
