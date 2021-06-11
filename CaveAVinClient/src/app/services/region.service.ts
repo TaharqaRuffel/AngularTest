52e3c62a1ecef418125f310545437431fd4e5fcb
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-06-11 14:42:03 +0200

    création du service region (CRUD)

---
+++ CaveAVinClient/src/app/services/region.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {Region} from "../modeles/region";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegionService {
+  regions = [
+    new Region( 1 ,'Pays de la Loire'),
+    new Region( 2 ,'Bretagne'),
+    new Region( 3 ,'Bordeaux'),
+    new Region( 4 ,'Alsace'),
+    new Region( 5 ,'Bourgogne')
+  ]
+
+  constructor() { }
+
+  getRegions(){
+    return this.regions;
+  }
+
+  getRegion(index:number):Region{
+    return this.regions[index];
+  }
+
+  deleteRegion(index:number):Region[]{
+    this.regions.splice(index,1);
+    return this.regions;
+  }
+
+  addRegion(newRegion:Region):Region[]{
+    this.regions.push(newRegion);
+    return this.regions;
+  }
+
+  editRegion(index:number,modifiedRegion:Region):Region[]{
+    this.regions[index] = modifiedRegion;
+    return this.regions;
+  }
+
+}
