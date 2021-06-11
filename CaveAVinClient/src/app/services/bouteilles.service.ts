0abb082a6ec9a8dec6ac6a4d92a1b9da7f23a1bf
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-06-11 15:00:53 +0200

    Mise à jour du service couleur (CRUD)

--- CaveAVinClient/src/app/services/bouteilles.service.ts
+++ CaveAVinClient/src/app/services/bouteilles.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Bouteille} from "../modeles/bouteille";
+import {RegionService} from "./region.service";
+import {CouleurService} from "./couleur.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BouteillesService {
 
-  bouteilles = [
-    {id : 1 ,nom:'Chateau Latour','couleur' : 'rouge', 'region':'Bordeaux','petillant':false,'millesime':'2014','quantite':3},
-    {id : 2 ,nom:'Coteaux du Layon','couleur' : 'blanc', 'region':'Pays de la Loire','petillant':true,'millesime':'2020','quantite':5}
-  ]
+  bouteilles = [] as any;
 
-  constructor() { }
+  constructor(private serviceRegion: RegionService,private serviceCouleur: CouleurService) {
+    this.bouteilles = [
+      new Bouteille(1 ,'Chateau Latour',serviceRegion.getRegion(3),serviceCouleur.getCouleur(1) ,false,'2014',3),
+      new Bouteille(2 ,'Coteaux du Layon',serviceRegion.getRegion(1),serviceCouleur.getCouleur(2)  ,true,'2020',5)
+    ]
+  }
 
   getBouteilles(){
     return this.bouteilles;
@@ -18,6 +23,20 @@ export class BouteillesService {
 
   deleteBouteille(index:number){
     this.bouteilles.splice(index,1);
+    return this.bouteilles;
+  }
+
+  getBouteille(index:number):Bouteille{
+    return this.bouteilles[index];
+  }
+
+  addBouteille(newBouteille:Bouteille):Bouteille[]{
+    this.bouteilles.push(newBouteille);
+    return this.bouteilles;
+  }
+
+  editBouteille(index:number,modifiedBouteille:Bouteille):Bouteille[]{
+    this.bouteilles[index] = modifiedBouteille;
     return this.bouteilles;
   }
 
