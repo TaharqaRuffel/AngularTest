fb98102a5c2d4a1668d22bada91c5c1b0d855eba
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-06-13 15:15:40 +0200

    Routage - création d'un lien dans la navbar vers le formulaire d'ajout d'une bouteille

--- CaveAVinClient/src/app/app-routing.module.ts
+++ CaveAVinClient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FormBouteilleComponent} from "./composants/formulaires/form-bouteille/form-bouteille.component";
+import {AccueilComponent} from "./composants/accueil/accueil.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bouteille/ajouter', component : FormBouteilleComponent},
+  {path: '', component : AccueilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
