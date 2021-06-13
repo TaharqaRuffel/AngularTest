91808464bcb45acda17b73b83226e6353d265e57
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-06-13 16:25:17 +0200

    Routage - création d'un lien dans la liste de bouteille vers la bouteille

--- CaveAVinClient/src/app/app-routing.module.ts
+++ CaveAVinClient/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {FormBouteilleComponent} from "./composants/formulaires/form-bouteille/form-bouteille.component";
 import {AccueilComponent} from "./composants/accueil/accueil.component";
 import {BouteillesComponent} from "./composants/bouteilles/bouteilles.component";
+import {BouteilleComponent} from "./composants/bouteille/bouteille.component";
 
 const routes: Routes = [
   {path: '', component : AccueilComponent},
-  {path: 'bouteille/liste', component : BouteillesComponent},
+  {path: 'bouteilles', component : BouteillesComponent},
+  {path: 'bouteilles/:id', component : BouteilleComponent},
   {path: 'bouteille/ajouter', component : FormBouteilleComponent},
 
 ];
--- CaveAVinClient/src/app/composants/bouteille/bouteille.component.ts
+++ CaveAVinClient/src/app/composants/bouteille/bouteille.component.ts
@@ -3,6 +3,7 @@ import {BouteillesService} from "../../services/bouteilles.service";
 import {Bouteille} from "../../modeles/bouteille";
 import {Region} from "../../modeles/region";
 import {Couleur} from "../../modeles/couleur";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-bouteille',
@@ -12,11 +13,21 @@ import {Couleur} from "../../modeles/couleur";
 export class BouteilleComponent implements OnInit {
 
   @Input() bouteille:Bouteille = new Bouteille( 0 ,'Pas de bouteille',new Region(0,''),new Couleur(0,''),false,'2021',0);
-  @Input()index = 0;
+  @Input() index = 0;
 
-  constructor(private serviceBouteilles: BouteillesService ) { }
+  constructor(private serviceBouteilles: BouteillesService,private routeActive: ActivatedRoute) { }
 
   ngOnInit(): void {
+    if(this.index === 0 ){
+      let idBouteille = this.routeActive.snapshot.paramMap.get('id');
+      for (let bouteille of this.serviceBouteilles.getBouteilles()) {
+        if (idBouteille == bouteille.id){
+          this.bouteille = bouteille;
+          break;
+        }
+
+      }
+    }
   }
 
   supprimerBouteille(){
