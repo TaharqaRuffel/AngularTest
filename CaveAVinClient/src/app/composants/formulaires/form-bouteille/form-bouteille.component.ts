f58ee6bc96dda8ee4fc55238afedf4e7a78b7797
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-06-23 13:53:12 +0200

    Modification de form-bouteille.component.ts pour gestion de l'édition en plus de l'addition

--- CaveAVinClient/src/app/composants/formulaires/form-bouteille/form-bouteille.component.ts
+++ CaveAVinClient/src/app/composants/formulaires/form-bouteille/form-bouteille.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Bouteille} from "../../../modeles/bouteille";
 import {RegionService} from "../../../services/region.service";
@@ -6,6 +6,7 @@ import {BouteillesService} from "../../../services/bouteilles.service";
 import {CouleurService} from "../../../services/couleur.service";
 import {Couleur} from "../../../modeles/couleur";
 import {Region} from "../../../modeles/region";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-form-bouteille',
@@ -13,6 +14,8 @@ import {Region} from "../../../modeles/region";
   styleUrls: ['./form-bouteille.component.css']
 })
 export class FormBouteilleComponent implements OnInit, OnDestroy {
+  @Input() bouteille: Bouteille = new Bouteille();
+  @Input() isUpdate: boolean = false;
 
   bouteilleForm = new FormGroup({
     id: new FormControl(''),
@@ -39,19 +42,47 @@ export class FormBouteilleComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.serviceCouleur.getCouleurs().toPromise().then((data) =>this.listeCouleurs = data);
-    this.serviceRegion.getRegions().subscribe((data)=> this.listeRegions = data);
+    this.serviceCouleur.getCouleurs().toPromise().then((data) => this.listeCouleurs = data);
+    this.serviceRegion.getRegions().subscribe((data) => this.listeRegions = data);
+    if (this.isUpdate) {
+      this.bouteilleForm.setValue({
+        id: this.bouteille.id,
+        nom: this.bouteille.nom,
+        region: this.bouteille.nom,
+        couleur: this.bouteille.couleur?.id,
+        petillant: this.bouteille.petillant,
+        millesime: this.bouteille.region?.id,
+        quantite: this.bouteille.quantite,
+      })
+    }
+
   }
 
-  onSubmit(){
-    if(this.bouteilleForm.valid){
-      this.ajoutBouteille(this.bouteilleForm.value);
+  onSubmit() {
+
+    let regionBouteille: Region = new Region();
+    let couleurBouteille: Couleur = new Couleur();
+    let bouteille: Bouteille = this.bouteilleForm.value;
+    regionBouteille.id = this.bouteilleForm.value.region;
+    couleurBouteille.id = this.bouteilleForm.value.couleur;
+    bouteille.region = regionBouteille;
+    bouteille.couleur = couleurBouteille;
+    if (this.bouteilleForm.valid) {
+      if (this.isUpdate) {
+        this.modifierBouteille(bouteille);
+      } else {
+        this.ajoutBouteille(bouteille);
+      }
+      this.router.navigate(['bouteilles'])
     }
   }
 
-  ajoutBouteille(bouteille:Bouteille):void{
-    this.serviceBouteille.addBouteille(bouteille);
-    console.log(this.serviceBouteille.getBouteilles());
+  ajoutBouteille(bouteille: Bouteille):void {
+    this.serviceBouteille.addBouteille(bouteille).subscribe();
+  }
+
+  modifierBouteille(bouteille: Bouteille):void {
+    this.serviceBouteille.editBouteille(bouteille).subscribe();
   }
 
   switchFormCouleur(): void {
